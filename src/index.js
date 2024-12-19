6156b9da4046c827edc2295b3dd02e813329d294
=chrixjohn <=chrisjon100@gamil.com>
2024-12-20 01:10:49 +0530

    setting model and basic api route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+const {userRoutes,expertRoutes} = require('./routes');
+const { logger } = require("./middlewares");
 require('dotenv').config();
+
 const app = express();
+const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI;
 
-const port = process.env.PORT
+app.use(logger);
+app.use(express.json());
+app.use(cors());  // Middleware to parse JSON request body
 
+app.use('/user', userRoutes);
+app.use('/expert', expertRoutes);  // Use user routes under the '/api' path
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+mongoose.connect(mongoURI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('Error connecting to MongoDB:', err));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
