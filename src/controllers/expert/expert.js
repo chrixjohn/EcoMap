5a7511c17a6c131387105b5d118304fab73fc72d
=chrixjohn <chrisjon100@gmail.com>
2025-01-14 22:59:19 +0530

    minor changes

--- src/controllers/expert/expert.js
+++ src/controllers/expert/expert.js
@@ -29,7 +29,7 @@ async function loginexpert(req, res) {
   try {
     const user = await User.findOne({ email });
     
-    if (!user) return res.status(404).json({ message: 'User not found' });
+    if (!user) return res.status(401).json({ message: 'User not found' });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
