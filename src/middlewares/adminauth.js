8f01f2f2e45c5149d2f4d648367977f25bb80c29
alanjuice <alnjoze@gmail.com>
2025-02-11 14:57:45 +0530

    add role based authorization

--- src/middlewares/adminauth.js
+++ src/middlewares/adminauth.js
@@ -2,23 +2,28 @@ const jwt = require("jsonwebtoken");
 const Admin = require("../models/adminModel");
 
 async function adminauth(req, res, next) {
-  const token = req.headers["x-authtoken"];
+    const token = req.headers["x-authtoken"];
 
-  if (!token) {
-    return res.status(401).json({ message: "No token provided" });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Use the secret from .env
-    const admin = await Admin.findOne({ _id: decoded.id });
-    if (!admin) {
-      return res.status(401).json({ message: "Admin not found please register" });
+    if (!token) {
+        return res.status(401).json({ message: "No token provided" });
     }
-    req.user = decoded; // Attach decoded user data to the request object
-    next(); // Proceed to the next middleware or route handler
-  } catch (error) {
-    return res.status(403).json({ message: "Invalid token" });
-  }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (decoded.role != "admin") {
+            return res.status(403).json({ message: "Invalid token" });
+        } // Use the secret from .env
+        const admin = await Admin.findOne({ _id: decoded.id });
+        if (!admin) {
+            return res
+                .status(401)
+                .json({ message: "Admin not found please register" });
+        }
+        req.user = decoded; // Attach decoded user data to the request object
+        next(); // Proceed to the next middleware or route handler
+    } catch (error) {
+        return res.status(403).json({ message: "Invalid token" });
+    }
 }
 
 module.exports = adminauth;
--- src/middlewares/expertauth.js
+++ src/middlewares/expertauth.js
@@ -2,23 +2,28 @@ const jwt = require("jsonwebtoken");
 const Expert = require("../models/expertModel");
 
 async function expertauth(req, res, next) {
-  const token = req.headers["x-authtoken"];
+    const token = req.headers["x-authtoken"];
 
-  if (!token) {
-    return res.status(401).json({ message: "No token provided" });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Use the secret from .env
-    const expert = await Expert.findOne({ _id: decoded.id });
-    if (!expert) {
-      return res.status(401).json({ message: "Expert not found please register" });
+    if (!token) {
+        return res.status(401).json({ message: "No token provided" });
     }
-    req.user = decoded; // Attach decoded user data to the request object
-    next(); // Proceed to the next middleware or route handler
-  } catch (error) {
-    return res.status(403).json({ message: "Invalid token" });
-  }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Use the secret from .env
+        if (decoded.role != "expert") {
+            return res.status(403).json({ message: "Invalid token" });
+        } // Use the secret from .env
+        const expert = await Expert.findOne({ _id: decoded.id });
+        if (!expert) {
+            return res
+                .status(401)
+                .json({ message: "Expert not found please register" });
+        }
+        req.user = decoded; // Attach decoded user data to the request object
+        next(); // Proceed to the next middleware or route handler
+    } catch (error) {
+        return res.status(403).json({ message: "Invalid token" });
+    }
 }
 
 module.exports = expertauth;
